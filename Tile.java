1688186e628d6534a0a3d473802f74e155d3e214
bn010001 <161021956+bn010001@users.noreply.github.com>
2024-07-17 12:10:51 +0200

    Add files via upload

---
+++ Tile.java
@@ -0,0 +1,18 @@
+package gameworld;
+
+
+import java.awt.image.BufferedImage;
+
+
+public class Tile {
+
+    public BufferedImage image;
+    public boolean collisione=false;
+
+
+    public Tile(){
+        
+    }
+
+    
+}
